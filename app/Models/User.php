9492f849231867a2f8e7d48f61902af9c639e200
unknown <masterionic@gmail.com>
2018-06-04 11:13:31 +0800

    config

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ class User extends Authenticatable
     use Notifiable;
 
     protected $table = 'tb_user';
-    public $timestamps = false;
+
 
     /**
      * The attributes that are mass assignable.
