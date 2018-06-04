de7c9ebaecc5c1d578d59277a953365a2c0f3381
unknown <masterionic@gmail.com>
2018-06-04 11:26:22 +0800

    config

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/Home';
 
     /**
      * Create a new controller instance.
