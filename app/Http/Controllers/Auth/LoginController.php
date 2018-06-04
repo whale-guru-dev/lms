2330a1385b6c08d79ad13215fbdc74ba558703b9
unknown <masterionic@gmail.com>
2018-06-04 14:51:13 +0800

    enrol

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,7 +38,10 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-
+    public function username()
+    {
+        return 'username';
+    }
 
 
 }
